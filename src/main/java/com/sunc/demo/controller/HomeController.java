4849415595c2275a3a9a8bd0a285d5eeb7f7af04
nisarab198 <nisarab198@INPC1986LZ.IN.nam.ad.pwcinternal.com>
2020-02-27 15:08:15 +0530

    new urlchange

--- src/main/java/com/sunc/demo/controller/HomeController.java
+++ src/main/java/com/sunc/demo/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.sunc.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
 public class HomeController {
 
 	
-	@GetMapping(value="/")
+	@RequestMapping(value="/")
 	public String sayHello() {
 		return "Hellu";
 	}
