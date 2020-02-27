cb40c2a5122eac325f546b2696290ad1a20487e9
nisarab198 <nisarab198@INPC1986LZ.IN.nam.ad.pwcinternal.com>
2020-02-27 11:08:30 +0530

    new change for autoconfig

--- src/main/java/com/sunc/demo/WebTokenApplication.java
+++ src/main/java/com/sunc/demo/WebTokenApplication.java
@@ -1,11 +1,13 @@
 package com.sunc.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class WebTokenApplication extends SpringBootServletInitializer {
 	 
 	  
