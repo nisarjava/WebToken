e2aa6bdca7a1d67b00963b194ff6ac82a8e5ba5a
nisarab198 <nisarab198@INPC1986LZ.IN.nam.ad.pwcinternal.com>
2020-02-27 12:08:16 +0530

    new change

--- src/main/java/com/sunc/demo/controller/HomeController.java
+++ src/main/java/com/sunc/demo/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	
-	@GetMapping(name = "hello", path = "/")
+	@GetMapping(value="/")
 	public String sayHello() {
 		return "Hellu";
 	}
